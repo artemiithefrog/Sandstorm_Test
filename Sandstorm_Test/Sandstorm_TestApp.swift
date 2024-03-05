ac34f77a35da070aafb732ee8a06cdd0d1aefa9c
Artemii <artemiithefrog95@icloud.com>
2024-03-05 23:24:58 +0100

    Initial Commit

---
+++ Sandstorm_Test/Sandstorm_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sandstorm_TestApp.swift
+//  Sandstorm_Test
+//
+//  Created by artemiithefrog . on 05.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct Sandstorm_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
