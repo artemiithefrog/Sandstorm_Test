4f46865956f66308f96d2893e473197f7b399ffa
Artemii <artemiithefrog95@icloud.com>
2024-03-13 16:06:36 +0100

    Created ContentView

---
+++ Sandstorm_Test/Views/ContentView.swift
@@ -0,0 +1,52 @@
+//
+//  ContentView.swift
+//  Sandstorm_Test
+//
+//  Created by artemiithefrog . on 05.03.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @StateObject var viewModel = ContentViewModel()
+    
+    var body: some View {
+        ZStack {
+            Color.gray.ignoresSafeArea()
+            GeometryReader { geometry in
+                VStack {
+                    ImageView(imageName: "panda", geometry: geometry, shouldFillWholeScreen: false)
+                    HStack(spacing: 20) {
+                        CustomButton(title: viewModel.lockTopButton ? "Unlock" : "Lock") {
+                            viewModel.lockTopButton.toggle()
+                        }
+                        .padding(.leading, 20)
+                        CustomButton(title: "Open from top") {
+                            withAnimation(.easeInOut) {
+                                viewModel.isDetailViewPresented.toggle()
+                            }
+                        }
+                        .opacity(viewModel.lockTopButton ? 0.5 : 1)
+                        .disabled(viewModel.lockTopButton)
+                        .padding(.trailing, 20)
+                    }
+                    Spacer()
+                    CustomButton(title: "Open full") {
+                        
+                    }
+                    .padding(.bottom, 20)
+                    .frame(maxWidth: geometry.size.width - 40)
+                }
+            }
+        }
+        .overlay {
+            if viewModel.isDetailViewPresented {
+                DetailViewModel(isPresented: $viewModel.isDetailViewPresented).view
+            }
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
